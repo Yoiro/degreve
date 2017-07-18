8b61ea24b08f9a0fd0dbcc90c836a9a0de3419ef
Alastard <xerud2003@hotmail.com>
2017-07-18 14:48:23 +0200

    Modif imports

--- app/src/main/java/be/helha/degreve/Serialization/LivreDeserializer.java
+++ app/src/main/java/be/helha/degreve/Serialization/LivreDeserializer.java
@@ -1,17 +1,8 @@
 package be.helha.degreve.Serialization;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
 
 import java.io.Serializable;
-import java.lang.reflect.Type;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 import be.helha.degreve.Entities.Livre;
 
